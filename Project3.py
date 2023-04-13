087d153783c9a2644037b2f6664174ae8710f2e3
2Aram1Hakobyan2 <ar2hakobyan1@yahoo.com>
2023-04-13 19:34:12 +0400

    Project 3 is done

---
+++ Project3.py
@@ -0,0 +1,25 @@
+def convertet():
+    hours = Seconds//3600
+    minutes = (Seconds//60)%60
+    ss = Seconds%60
+    print("Time Left - ",hours,":",minutes , ":", ss)
+
+import random
+import time
+phrase = ["Set a timer", "Timer variables should be inserted", "Countdown should be set"]
+rndphrase = random.choice(phrase)
+print(rndphrase)
+h = int(input('Hours : '))
+m = int(input('Minutes : '))
+s = int(input('Seconds : '))
+Seconds = h*3600+m*60+s
+while Seconds!=0:
+    minutes = Seconds//60
+    ss = Seconds%60
+    time.sleep(1)
+    convertet()
+    Seconds-=1
+    if Seconds==0:
+        print("Time Left -  0 : 0 : 0")
+        print("Time is Up!!! ")
+        break
